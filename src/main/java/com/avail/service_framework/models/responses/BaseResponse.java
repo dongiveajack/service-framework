52f91034159481ba4a44b1070b0f6df2bda75796
Abhinav <tripathi.abhinav01@gmail.com>
2021-10-27 12:24:00 +0530

    added superbuilder at base respi

--- src/main/java/com/avail/service_framework/models/responses/BaseResponse.java
+++ src/main/java/com/avail/service_framework/models/responses/BaseResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 
@@ -11,7 +12,7 @@ import java.io.Serializable;
  * Created By Abhinav Tripathi
  */
 @Data
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class BaseResponse implements Serializable {
