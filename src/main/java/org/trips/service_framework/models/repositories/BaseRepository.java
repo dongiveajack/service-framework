466dbe8a9bd4112f83455999289a796dd1511c5b
Abhinav Tripathi <abhinav.tripathi@captainfresh.in>
2022-07-15 10:01:50 +0530

    extended baseRepository with QuerydslPredicateExecutor

--- src/main/java/org/trips/service_framework/models/repositories/BaseRepository.java
+++ src/main/java/org/trips/service_framework/models/repositories/BaseRepository.java
@@ -1,5 +1,6 @@
 package org.trips.service_framework.models.repositories;
 
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.trips.service_framework.models.entities.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 /**
  * Created By Abhinav Tripathi
  */
-public interface BaseRepository<M extends BaseEntity> extends JpaRepository<M, Long>, JpaSpecificationExecutor<M> {
+public interface BaseRepository<M extends BaseEntity> extends JpaRepository<M, Long>, JpaSpecificationExecutor<M>, QuerydslPredicateExecutor<M> {
 }
