0b34ac834d413de444d532c37acbd2305591f5eb
Anomitra <anomitra.saha@captainfresh.in>
2025-06-17 14:27:30 +0530

    FIX: Add authentication to audit data endpoint

--- src/main/java/org/trips/service_framework/audit/controllers/AuditLogController.java
+++ src/main/java/org/trips/service_framework/audit/controllers/AuditLogController.java
@@ -2,6 +2,7 @@ package org.trips.service_framework.audit.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpStatus;
+import org.trips.service_framework.aop.Authenticate;
 import org.trips.service_framework.audit.requests.AuditRequest;
 import org.trips.service_framework.audit.responses.AuditResponse;
 import org.trips.service_framework.audit.services.AuditLogService;
@@ -25,6 +26,7 @@ public class AuditLogController {
     private final AuditLogService auditLogService;
 
     @PostMapping("/changes")
+    @Authenticate
     public AuditResponse audit(@RequestBody AuditRequest request) {
         var data = auditLogService.getAuditLogs(request.getEntityId(), request.getEntityClass());
         return AuditResponse.builder()
